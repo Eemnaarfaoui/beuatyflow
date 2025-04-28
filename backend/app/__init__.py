74e5e3099aef46b11cfd12a6acc6224324b53c54
chaymaboubakri <96527065+chaymaboubakri@users.noreply.github.com>
2025-04-28 19:03:55 +0100

    crud for warehouses+inventory

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,29 +2,42 @@ from flask import Flask
 from flask_cors import CORS
 from sqlalchemy import create_engine
 import dns.resolver
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app)  # Cela permet de résoudre les problèmes CORS pendant le développement.
 
-    # Connection config
-    server = "BI"
-    database = "DW_Supply_Chain"
+    # Configuration de la connexion à la base de données
+    server = "CHAYMA_BOUBAKRI\BI"
+    database_DW = "DW_Supply_Chain"
+    database = "SA_Supply_Chain"
     driver = "ODBC Driver 17 for SQL Server"
 
-    # SQLAlchemy engine
+    # Configuration de la connexion SQLAlchemy
     connection_str = f"mssql+pyodbc://{server}/{database}?trusted_connection=yes&driver={driver.replace(' ', '+')}"
+
     app.config['SQL_ENGINE'] = create_engine(connection_str)
+
+    # Configuration de MongoDB et du secret JWT
     app.config['MONGO_URI'] = "mongodb+srv://Emna:1011@beautyflow.cpfshru.mongodb.net/"
     app.config['JWT_SECRET_KEY'] = 'super-secret-key'
-    # Register routes
+
+    # Enregistrement des routes
     from .routes.sales_routes import init_sales_routes
     init_sales_routes(app)
- 
+
     from .routes.user_routes import init_user_routes
     init_user_routes(app)
 
-    from flask_jwt_extended import JWTManager
+    from .routes.inventory_routes import init_inventory_routes
+    init_inventory_routes(app)
+
+    # Ajouter les routes des entrepôts
+    from .routes.warehouse_routes import init_warehouse_routes
+    init_warehouse_routes(app)
+
+    # Configuration de JWT
     jwt = JWTManager(app)
 
     return app
