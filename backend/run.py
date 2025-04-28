54563a6d1de1fd56372c312e0c26e879a1ff67b0
chaymaboubakri <96527065+chaymaboubakri@users.noreply.github.com>
2025-04-28 19:03:55 +0100

    crud for warehouses+inventory

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,11 @@
-# run.py
-from app import create_app
+from app import create_app  # Importe la fonction create_app de ton application Flask
 
+# Créer l'application Flask en appelant la fonction create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Exécution de l'application avec les options suivantes :
+    # - debug=True pour activer le mode débogage (affichage d'erreurs détaillées)
+    # - host='0.0.0.0' pour permettre l'écoute sur toutes les interfaces réseau
+    # - port=5000 pour spécifier le port de l'application
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- frontend/src/app/pages/storage/templates/powerbi.ts
+++ frontend/src/app/pages/storage/templates/powerbi.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-storage-powerbi',
-  imports: [],
   template: `
-  <div class="grid grid-cols-12 gap-8">
-   Storage Dashboards
-  </div>`,
+    <div class="grid grid-cols-12 gap-8">
+      <div class="col-span-12">
+        <iframe 
+          [src]="safePowerBIUrl" 
+          frameborder="0" 
+          allowfullscreen="true">
+        </iframe>
+      </div>
+    </div>
+  `,
+  styles: [`
+    iframe {
+      border: none;
+      width: 100%;
+      height: 100vh; /* Hauteur = toute la hauteur de l'écran */
+      display: block;
+      margin: 0 auto;
+    }
+  `]
 })
 export class PowerBiComponent {
+  powerBIUrl: string = 'https://app.powerbi.com/reportEmbed?reportId=f6e55757-1a41-44db-ac19-94f030d27931&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730';
+  safePowerBIUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.safePowerBIUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.powerBIUrl);
+  }
 }
+
