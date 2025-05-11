170592ac0bfbf198da0e803d1f99cbc3e1406282
Emna Arfaoui <101388686+Eemnaarfaoui@users.noreply.github.com>
2025-05-11 18:24:23 +0100

    removed unused files and pycache

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 # config.py or inside create_app function in __init__.py
 class Config:
-    SERVER = "DESKTOP-CAV7GGJ\MSSQLSERVERRR"
-    DATAWAREHOUSE = "DW_FINALE"
-    STAGING_AREA ="SA_SupplyChain"
+    SERVER = "localhost"
+    DATAWAREHOUSE = "DW_SupplyChain"
+    STAGING_AREA ="SA_Supply_Chaine"
     DRIVER = "ODBC Driver 17 for SQL Server"
     MONGO_URI = "mongodb+srv://Emna:1011@beautyflow.cpfshru.mongodb.net/"
